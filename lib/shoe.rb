571f241bba2635ccbcc55a6d8ff6a5657c7d6d61
Mike Starr <imhs@protonmail.com>
2022-02-24 12:27:19 -0800

    Completed assignment

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,36 @@
+require 'pry'
 # Make your shoe class here!
+class Shoe
+
+    attr_reader :brand
+    attr_accessor :color, :size, :material,:condition
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def color
+        @color
+    end
+
+    def size
+        @size
+    end
+
+    def material
+        @material
+    end
+
+    def condition
+        @condition
+    end
+
+    def cobble
+        if @condition != "new"
+        puts "Your shoe is as good as new!"
+            @condition = "new"
+        end
+    end
+end
+
+# binding.pry
